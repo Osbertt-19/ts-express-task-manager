e6f3f5fcf0f4339e37fe1301af79270d1ffa632b
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-08-05 10:22:16 +0700

    added morgan

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import './database'; // initialize database
 import routes from './routes';
 import errorHandler from './error/errorHandler';
 import notFound from './error/notFound';
+import morgan from 'morgan';
 
 process.on('uncaughtException', (e) => {
   Logger.error(e);
@@ -14,10 +15,10 @@ process.on('uncaughtException', (e) => {
 
 const app = express();
 
+app.use(morgan('tiny'));
+
 app.use(express.json({ limit: '10mb' }));
-app.use(
-  express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }),
-);
+app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
 app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
 
 // Routes
